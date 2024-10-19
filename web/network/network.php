268782e85c5c1c62ca65a794b504b31607c79eed
Caner Koyuncu <16canerkoyuncu16@gmail.com>
2024-10-19 18:13:03 +0300

    aaaaaAAAAA

---
+++ web/network/network.php
@@ -0,0 +1,18 @@
+<?php
+
+function api_post($path, $data)
+{
+    $url = "http://127.0.0.1:8080" . $path;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    header('Access-Control-Allow-Origin: *');
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $response = json_decode($response, false);
+    $response = json_decode($response, false);
+
+    return $response;
+}
